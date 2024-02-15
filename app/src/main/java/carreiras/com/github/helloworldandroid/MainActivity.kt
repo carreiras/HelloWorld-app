e76beff9a016aaffed64b765a234b6b4bfb60f91
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-15 20:13:27 -0300

    Criando uma activity_main

--- app/src/main/java/carreiras/com/github/helloworldandroid/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/helloworldandroid/MainActivity.kt
@@ -9,11 +9,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Criando um objeto de texto
-        val texto = TextView(this)
-        texto.text = "Hello Kotlin"
-
-        //definindo o conteúdo da tela
-        setContentView(texto)
+        // Definindo o conteúdo da tela
+        setContentView(R.layout.activity_main)
     }
 }
